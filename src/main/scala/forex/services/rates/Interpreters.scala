0bd4d435b676b9946c48b6d50c836ead4d928a72
Anton Serhiienko <anton.serhiienko@klarna.com>
2021-01-30 12:36:46 +0200

    adding cache

--- src/main/scala/forex/services/rates/Interpreters.scala
+++ src/main/scala/forex/services/rates/Interpreters.scala
@@ -1,8 +1,16 @@
 package forex.services.rates
 
-import cats.Applicative
-import interpreters._
+import cats.FlatMap
+import cats.effect.concurrent.Ref
+import cats.effect.{Clock, Sync}
+import forex.config.OneFrameConfig
+import forex.domain.Rate
+import forex.services.rates.interpreters._
+import org.http4s.client.Client
+
+import scala.collection.immutable.HashMap
 
 object Interpreters {
-  def dummy[F[_]: Applicative](): Algebra[F] = new OneFrameDummy[F]()
+  def cached[F[_]: FlatMap: Clock](cache: Ref[F, HashMap[Rate.Pair, Rate]]): Algebra[F] = new OneFrameCache[F](cache)
+  def http[F[_]: Sync](config: OneFrameConfig,http: Client[F]): Algebra[F] = new OneFrameHttp[F](config, http)
 }
---
+++ src/main/scala/forex/services/rates/interpreters/OneFrameCache.scala
@@ -0,0 +1,29 @@
+package forex.services.rates.interpreters
+
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.concurrent.TimeUnit
+
+import cats.FlatMap
+import cats.effect.Clock
+import cats.effect.concurrent.Ref
+import cats.syntax.either._
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+import forex.domain.Rate
+import forex.services.rates.Algebra
+import forex.services.rates.errors.Error.OneFrameLookupFailed
+import forex.services.rates.errors._
+
+import scala.collection.immutable.HashMap
+
+class OneFrameCache[F[_]: FlatMap : Clock](db: Ref[F, HashMap[Rate.Pair, Rate]]) extends Algebra[F] {
+
+  override def get(pair: Rate.Pair): F[Error Either Rate] = Clock[F].realTime(TimeUnit.MILLISECONDS).flatMap{ now =>
+    db.get.map{cache => cache.get(pair)}.map{
+      case Some(x) if x.timestamp.value.toInstant.isAfter(Instant.ofEpochMilli(now).minus(5, ChronoUnit.MINUTES)) => x.asRight
+      case _ => OneFrameLookupFailed("Can't find exchange rate right now").asLeft
+    }
+  }
+  override def getMany(pairs: Set[Rate.Pair]): F[List[Rate]] = db.get.map{_.values.toList}
+}
