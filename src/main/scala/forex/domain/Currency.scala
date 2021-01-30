dd65e46c445172651dd23ae52a1ba5f54d45c6f7
Anton Serhiienko <anton.serhiienko@klarna.com>
2021-01-30 12:40:34 +0200

    adding enumeratum

--- src/main/scala/forex/domain/Currency.scala
+++ src/main/scala/forex/domain/Currency.scala
@@ -1,42 +1,29 @@
 package forex.domain
 
 import cats.Show
+import enumeratum.EnumEntry
+import enumeratum._
 
-sealed trait Currency
+import scala.collection.immutable
 
-object Currency {
-  case object AUD extends Currency
-  case object CAD extends Currency
-  case object CHF extends Currency
-  case object EUR extends Currency
-  case object GBP extends Currency
-  case object NZD extends Currency
-  case object JPY extends Currency
-  case object SGD extends Currency
-  case object USD extends Currency
+sealed abstract class Currency(val value: String) extends EnumEntry
 
-  implicit val show: Show[Currency] = Show.show {
-    case AUD => "AUD"
-    case CAD => "CAD"
-    case CHF => "CHF"
-    case EUR => "EUR"
-    case GBP => "GBP"
-    case NZD => "NZD"
-    case JPY => "JPY"
-    case SGD => "SGD"
-    case USD => "USD"
-  }
+object Currency extends Enum[Currency] {
+  val values: immutable.IndexedSeq[Currency] = findValues
 
-  def fromString(s: String): Currency = s.toUpperCase match {
-    case "AUD" => AUD
-    case "CAD" => CAD
-    case "CHF" => CHF
-    case "EUR" => EUR
-    case "GBP" => GBP
-    case "NZD" => NZD
-    case "JPY" => JPY
-    case "SGD" => SGD
-    case "USD" => USD
-  }
+  case object AUD extends Currency("AUD")
+  case object CAD extends Currency("CAD")
+  case object CHF extends Currency("CHF")
+  case object EUR extends Currency("EUR")
+  case object GBP extends Currency("GBP")
+  case object NZD extends Currency("NZD")
+  case object JPY extends Currency("JPY")
+  case object SGD extends Currency("SGD")
+  case object USD extends Currency("USD")
+
+  implicit val show: Show[Currency] = Show.show {_.value}
+
+  def fromString(s: String): Option[Currency] = values.find(_.value.toUpperCase == s.toUpperCase)
 
 }
+
