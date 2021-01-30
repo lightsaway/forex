ea10827201abbed0f5babeaff461d40264fe3c26
Anton Serhiienko <anton.serhiienko@klarna.com>
2021-01-30 12:45:18 +0200

    adding safe query decoder

--- src/main/scala/forex/http/rates/QueryParams.scala
+++ src/main/scala/forex/http/rates/QueryParams.scala
@@ -1,13 +1,15 @@
 package forex.http.rates
 
 import forex.domain.Currency
-import org.http4s.QueryParamDecoder
+import org.http4s.{ParseFailure, QueryParamDecoder, QueryParameterValue}
 import org.http4s.dsl.impl.QueryParamDecoderMatcher
+import cats.implicits._
 
 object QueryParams {
 
   private[http] implicit val currencyQueryParam: QueryParamDecoder[Currency] =
-    QueryParamDecoder[String].map(Currency.fromString)
+    (value: QueryParameterValue) => QueryParamDecoder[String].decode(value)
+      .andThen(s => Currency.fromString(s).toValidNel(ParseFailure("invalid currency provided", "")))
 
   object FromQueryParam extends QueryParamDecoderMatcher[Currency]("from")
   object ToQueryParam extends QueryParamDecoderMatcher[Currency]("to")
